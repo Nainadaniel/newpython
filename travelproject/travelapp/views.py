688b92e9ac35cca1c6b67862a3f0b9868e002a0c
Nainadaniel <nainadaniel14@gmail.com>
2022-08-24 12:34:31 +0530

    added

---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from.models import place
+
+
+def demo(request):
+    obj=place.objects.all()
+    return render(request,"index.html",{'result':obj})
+
+
+
+
+
+
